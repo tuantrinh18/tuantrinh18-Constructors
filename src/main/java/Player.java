04dcf076555fd910c7de02c0c659c39c5abd8676
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-20 17:50:13 +0000

    lab1day2week2completed

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -30,8 +30,10 @@ public class Player {
      *
      * @param playerName the name of the player to be created.
      */
+    
     public Player(String playerName){
-
+        this.playerName = playerName;
+        level= 1;
     }
 
     /**
@@ -48,7 +50,10 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-
+        this.playerName=playerName;
+        this.level= level;
     }
+
+   
 
 }
